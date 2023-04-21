c5286023302c989f36d4c80533bd2f02a5db28b4
wantoper <780104525@qq.com>
2023-04-21 13:09:03 +0800

    User bean

---
+++ XiaojiDinner/src/main/java/com/wantoper/XiaoJi/Bean/User.java
@@ -0,0 +1,17 @@
+package com.wantoper.XiaoJi.Bean;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.Data;
+
+@Data
+public class User {
+    @TableId(type = IdType.ASSIGN_ID)
+    private String id;
+    private String name;
+    private String phone;
+    private String sex;
+    private String avatar;
+    private int status;
+    private String password;
+}
